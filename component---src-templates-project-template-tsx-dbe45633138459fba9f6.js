0ba0aaa1ff49fabfc49db1d07ff5d614bd441363
haiarha <haiarha@users.noreply.github.com>
2023-11-11 02:01:35 +0000

    deploy: 7549815fde9222ac02294a7a8cf6e864b505c1ae

---
+++ component---src-templates-project-template-tsx-dbe45633138459fba9f6.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[246],{5853:function(e,t,n){n.r(t);var l=n(7294);t.default=e=>{let{pageContext:t}=e;const{title:n,content:r}=t.data;return l.createElement("div",null,l.createElement("h1",null,n),l.createElement("div",{dangerouslySetInnerHTML:{__html:r}}))}}}]);
+//# sourceMappingURL=component---src-templates-project-template-tsx-dbe45633138459fba9f6.js.map
