8dc263f4b72f587b5927867d1514fb80ce16696d
Vadym Veremeichyk <vadym.veremeichyk@ext.eurostar.com>
2023-11-17 22:55:20 +0000

    add editorconfig

--- src/templates/project-template.tsx
+++ src/templates/project-template.tsx
@@ -1,16 +1,18 @@
-import React from "react"
-import { PageProps } from "gatsby"
-import Page from "../components/Page"
+import React from "react";
+import { PageProps } from "gatsby";
+import Page from "../components/Page";
 
-const PageTemplate: React.FC<PageProps<{}, ProjectContext<ProjectData>>> = ({ pageContext }) => {
-  const { title, content } = pageContext.data
+const PageTemplate: React.FC<PageProps<{}, ProjectContext<ProjectData>>> = ({
+  pageContext,
+}) => {
+  const { title, content } = pageContext.data;
 
   return (
     <Page>
       <h1>{title}</h1>
       <div dangerouslySetInnerHTML={{ __html: content }} />
     </Page>
-  )
-}
+  );
+};
 
-export default PageTemplate
+export default PageTemplate;
